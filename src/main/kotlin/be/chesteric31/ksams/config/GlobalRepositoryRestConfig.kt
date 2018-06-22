5bc90cd920bdbcc91052dfe64be218eb007c546b
chesteric31 <chesteric31@gmail.com>
2018-06-22 11:25:42 +0200

    Add allowed all methods for cors.

--- src/main/kotlin/be/chesteric31/ksams/config/GlobalRepositoryRestConfig.kt
+++ src/main/kotlin/be/chesteric31/ksams/config/GlobalRepositoryRestConfig.kt
@@ -11,6 +11,7 @@ class GlobalRepositoryRestConfig : RepositoryRestConfigurerAdapter() {
         config!!.corsRegistry
                 .addMapping("/api/**")
                 .allowedOrigins("*")
+                .allowedMethods("*")
     }
 
 }
