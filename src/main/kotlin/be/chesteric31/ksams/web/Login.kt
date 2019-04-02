71929d3524c50c8b547cb8cbf70c0b25ebf011bd
chesteric31 <chesteric31@gmail.com>
2019-04-02 21:20:37 +0200

    Add login and roles authentication.

---
+++ src/main/kotlin/be/chesteric31/ksams/web/Login.kt
@@ -0,0 +1,5 @@
+package be.chesteric31.ksams.web
+
+class Login(val username: String, val password: String, val roles: Array<String> = emptyArray()) {
+
+}
---
+++ src/main/kotlin/be/chesteric31/ksams/web/LoginController.kt
@@ -0,0 +1,33 @@
+package be.chesteric31.ksams.web
+
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import sun.security.acl.PrincipalImpl
+import java.security.Principal
+import java.util.*
+import javax.servlet.http.HttpServletRequest
+
+@RestController
+@RequestMapping("/api/login")
+class LoginController {
+
+    @PostMapping(value = ["/"])
+    fun login(@RequestBody login: Login): Login {
+        when {
+            "admin" == login.username && "admin" == login.password -> return Login("admin", "admin", arrayOf("ADMIN", "USER"))
+        }
+        when {
+            "user" == login.username && "user" == login.password -> return Login("user", "user", arrayOf("USER"))
+        }
+        throw Exception("No User found!")
+    }
+
+    @RequestMapping("/user")
+    fun user(request: HttpServletRequest): Principal {
+        val authToken = request.getHeader("Authorization").substring("Basic".length).trim { it <= ' ' }
+        return PrincipalImpl(String(Base64.getDecoder().decode(authToken)).split(":")[0])
+    }
+
+}
