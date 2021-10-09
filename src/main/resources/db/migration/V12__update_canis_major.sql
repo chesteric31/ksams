0a4de00d2531e9402c2857b523cff110400e9605
chesteric31 <eric.binard.182@gmail.com>
2021-10-09 09:02:43 +0200

    update canis_major image

--- src/main/resources/db/migration/V12__update_canis_major.sql
+++ src/main/resources/db/migration/V12__update_canis_major.sql
@@ -1,1 +1,1 @@
-UPDATE ARMOR_VERSION SET image = 'https://res.cloudinary.com/ksams/image/upload/v1633541867/silver/canis_maior_j0tiba.jpg' WHERE fk_armor = (select id from armor where name like 'Canis Major'));
+UPDATE ARMOR_VERSION SET image = 'https://res.cloudinary.com/ksams/image/upload/v1633541867/silver/canis_maior_j0tiba.jpg' WHERE fk_armor = (select id from armor where name like 'Canis Major');
