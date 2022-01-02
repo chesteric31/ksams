73d55f5318e06d73bd0a84d69ba5a57466055b42
chesteric31 <eric.binard.182@gmail.com>
2022-01-02 14:54:58 +0100

    update andromeda dark & pegasus dark image

---
+++ src/main/resources/db/migration/V13__update_armors_image.sql
@@ -0,0 +1,3 @@
+UPDATE ARMOR_VERSION SET image = 'https://res.cloudinary.com/ksams/image/upload/v1633541840/bronze/pegasus_dark_rrmiio.jpg' WHERE id = 2;
+UPDATE ARMOR_VERSION SET image = 'https://res.cloudinary.com/ksams/image/upload/v1633541857/bronze/andromeda_dark_jvhr8e.jpg' WHERE id = 14;
+
