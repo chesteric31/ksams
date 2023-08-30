fba91afa1e7721000c7de8272c531568f9a66272
chesteric31 <eric.binard.182@gmail.com>
2023-08-30 23:20:31 +0200

    fix tests

--- src/test/kotlin/be/chesteric31/ksams/KsamsApplicationTests.kt
+++ src/test/kotlin/be/chesteric31/ksams/KsamsApplicationTests.kt
@@ -1,50 +1,51 @@
 package be.chesteric31.ksams
 
-import org.junit.Before
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 
-@RunWith(SpringRunner::class)
-@ContextConfiguration(classes = [(KsamsApplication::class)])
 @ActiveProfiles("test")
 @SpringBootTest
 class KsamsApplicationTests {
 
-    lateinit var mockMvc : MockMvc
+    lateinit var mockMvc: MockMvc
 
     @Autowired
     lateinit var webApplicationContext: WebApplicationContext
 
-    @Before
-    fun setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
+    @Test
+    fun testCategoriesApi() {
+        mockMvc.perform(
+            MockMvcRequestBuilders.get("/api/v2/categories/")
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
     }
-
-	@Test
-	fun testCategoriesApi() {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/v2/categories/")
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk)
-	}
 
     @Test
     fun testCategoriesApiPost4xx() {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/v2/categories/")
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/api/v2/categories/")
                 .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is4xxClientError)
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().is4xxClientError)
+    }
+
+    companion object {
+        @JvmStatic
+        fun setup(ksamsApplicationTests: KsamsApplicationTests): Unit {
+            ksamsApplicationTests.mockMvc =
+                MockMvcBuilders.webAppContextSetup(ksamsApplicationTests.webApplicationContext).build()
+        }
     }
 
 }
