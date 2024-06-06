145beb01a077a96129dbc1055faade530afd78c3
chesteric31 <eric.binard.182@gmail.com>
2024-06-06 22:03:03 +0200

    fix test

--- src/test/kotlin/be/chesteric31/ksams/KsamsApplicationTests.kt
+++ src/test/kotlin/be/chesteric31/ksams/KsamsApplicationTests.kt
@@ -2,10 +2,13 @@ package be.chesteric31.ksams
 
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -14,9 +17,12 @@ import org.springframework.web.context.WebApplicationContext
 
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension::class)
 class KsamsApplicationTests {
 
-    lateinit var mockMvc: MockMvc
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
     @Autowired
     lateinit var webApplicationContext: WebApplicationContext
